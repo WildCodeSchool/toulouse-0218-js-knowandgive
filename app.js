01a8c7fc69385596d7891d8f447cc62058b69f67
Delannel <thomas.delannel@gmail.com>
2018-04-17 11:25:39 +0200

    modification de la serachbar + page résultat

--- app.js
+++ app.js
@@ -196,16 +196,42 @@ const charteGivemanHtml = `<div class="giveman">
           </button>
       </div>
 `
+const resultHtml = `<ul class="list-unstyled">
+  <li class="media">
+    <img class="mr-3" src="..." alt="Generic placeholder image">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+  <li class="media my-4">
+    <img class="mr-3" src="..." alt="Generic placeholder image">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+  <li class="media">
+    <img class="mr-3" src="..." alt="Generic placeholder image">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+</ul>
+`
+
 function resultKeyword(keyword) {
   return "resultats pour " + keyword
 }
 
-function showResultForKeyword(keyword) {
-  mainDiv.innerHTML = resultKeyword(keyword)
+function showResultForKeyword(keyword) { 
+  
+  render(searchbarHtml + resultHtml)
 }
 
 const render = mainHTML => {
-  mainDiv.innerHTML = navbarHtml + searchbarHtml + presentationHtml + competencesHtml + charteGivemanHtml + footerHtml
+  mainDiv.innerHTML = navbarHtml + mainHTML + footerHtml
 }
 
 
@@ -216,7 +242,7 @@ const search = () => {
 
  
 const home = () => {
-  render(navbarHtml)
+  render(searchbarHtml + presentationHtml + competencesHtml + charteGivemanHtml)
 
   const connexion = document.getElementById('form-post')
   connexion.addEventListener('submit', event => {

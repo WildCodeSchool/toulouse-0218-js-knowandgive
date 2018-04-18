fa02cd022cf2b801d2891138931c2468ac8df29f
Alexandre Morazin <alexandre.morazin@live.fr>
2018-04-18 17:34:26 +0200

    tentative d'implantation fonction photo

--- public/app.js
+++ public/app.js
@@ -251,10 +251,13 @@ const pagePersoHtml = /* @html */ `
 
        <div class="container">
            <div class="row">
-               <div class="col-md-6">
+               <div class="col-md-6 imgProfil">
                    <a href="https://placeholder.com"><img src="http://via.placeholder.com/200x200"></a>
                    <br>
-                   <button type="button" class="btn btn-light">Télécharger mon image</button>
+
+                   <!-- <input type="file" id="files" name="files[]" multiple />
+                   <output id="list"></output> -->
+
                </div>
                <div class="col-md-6">
                    <div class="card">
@@ -488,6 +491,54 @@ const home = () => {
 
   autocomplete(autocompleteInput, skill);
 }
+
+// function handleFileSelect(evt) {
+//     var files = evt.target.files; // FileList object
+//
+//     // files is a FileList of File objects. List some properties.
+//     var output = [];
+//     for (var i = 0, f; f = files[i]; i++) {
+//       output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+//                   f.size, ' bytes, last modified: ',
+//                   f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+//                   '</li>');
+//     }
+//     document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+//   }
+//
+//   document.getElementById('files').addEventListener('change', handleFileSelect, false);
+//
+//   function handleFileSelect(evt) {
+//       var files = evt.target.files; // FileList object
+//
+//       // Loop through the FileList and render image files as thumbnails.
+//       for (var i = 0, f; f = files[i]; i++) {
+//
+//         // Only process image files.
+//         if (!f.type.match('image.*')) {
+//           continue;
+//         }
+//
+//         var reader = new FileReader();
+//
+//         // Closure to capture the file information.
+//         reader.onload = (function(theFile) {
+//           return function(e) {
+//             // Render thumbnail.
+//             var span = document.createElement('span');
+//             span.innerHTML = ['<img class="thumb" src="', e.target.result,
+//                               '" title="', escape(theFile.name), '"/>'].join('');
+//             document.getElementById('list').insertBefore(span, null);
+//           };
+//         })(f);
+//
+//         // Read in the image file as a data URL.
+//         reader.readAsDataURL(f);
+//       }
+//     }
+//
+//     document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
 // home()
 //
 page('/', home)
